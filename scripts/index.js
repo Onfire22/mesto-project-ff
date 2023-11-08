061266da0c843f6a8c9634d7ad9fba5cd90eaa23
Maxim Necrasov <maddmax22@yandex.ru>
2023-11-08 14:55:52 +0300

    поправлены замечания

--- scripts/index.js
+++ scripts/index.js
@@ -3,20 +3,21 @@ const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 const cardList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-function cardInit (item) {
+function createCard (item, removeCard) {
   const cardElement = cardTemplate.cloneNode(true);
   cardElement.querySelector('.card__title').textContent = item.name;
   cardElement.querySelector('.card__delete-button').addEventListener('click', removeCard);
   cardElement.querySelector('.card__image').src = item.link;
+  cardElement.querySelector('.card__image').alt = item.name;
   return cardElement;
-};
+}
 // @todo: Функция удаления карточки
-function removeCard (evt) {
-  const eventTarget = evt.target;
-  eventTarget.parentElement.remove();
-};
+function removeCard () {
+  const removeBtn = document.querySelector('.card__delete-button');
+  removeBtn.closest('.card').remove();
+}
 // @todo: Вывести карточки на страницу
 initialCards.forEach(function(item) {
-  const card = cardInit(item, removeCard);
+  const card = createCard(item, removeCard);
   cardList.append(card);
-});
+})
